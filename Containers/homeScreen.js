a2359ac9a1c0feec42dfedfe613f3a41561e3605
kaba <kabaya.dori@gmail.com>
2018-07-26 21:19:08 +0900

    added card, next add s3

--- Containers/homeScreen.js
+++ Containers/homeScreen.js
@@ -4,73 +4,45 @@ import {
   View,
   Text,
   TextInput,
-  Button,
   FlatList
 } from 'react-native'
+import { Card, ListItem, Button } from 'react-native-elements'
 import firebase from 'react-native-firebase'
-import Todo from './todo' // we'll create this next
 
-import * as Animatable from 'react-native-animatable'
 
+const uri = 'http://cdn.buzz-plus.com/wp-content/uploads/2016/11/ramen-jiro-omiya6.jpg'
 class Home extends React.Component {
   constructor() {
     super()
-    this.ref = firebase.firestore().collection('todos')
-    this.unsubscribe = null
     this.state = {
-      textInput: '',
-      loading: true,
-      todos: [],
-      hidden: false,
       recipes: []
     }
   }
 
   // ラーメンの画像は以下を利用する。
-  // http://cdn.buzz-plus.com/wp-content/uploads/2016/11/ramen-jiro-omiya6.jpg
-
   componentDidMount = async () => {
-    this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate)
-    const recipes = await firebase.firestore().collection('cities').get()
-    console.log(recipes._docs[0]._data)
-    const newRecipes = recipes._docs.map(x => x._data)
-    console.log(newRecipes)
-    this.setState({ recipes: newRecipes })
+    const recipes = await firebase.firestore().collection('recipe').get()
+    console.log(recipes)
+    const newArray = recipes._docs.map(x => x._data)
+    console.log(newArray)
+    this.setState({ recipes: newArray })
   }
 
-  componentWillUnmount() {
-    this.unsubscribe()
-  }
-
-  onCollectionUpdate = querySnapshot => {
-    const todos = []
-    querySnapshot.forEach(doc => {
-      const { title, complete } = doc.data()
-      todos.push({
-        key: doc.id,
-        doc, // DocumentSnapshot
-        title,
-        complete
-      })
-    })
-    this.setState({
-      todos,
-      loading: false
-    })
-  }
-
-  updateTextInput(value) {
-    this.setState({ textInput: value })
-  }
-
-  addTodo() {
-    this.ref.add({
-      title: this.state.textInput,
-      complete: false
-    })
-    this.setState({
-      textInput: ''
-    })
+  renderItem = ({ item }) => {
+    return (
+      <Card
+        title={item.name}
+      >
+        <Text style={{ marginBottom: 10 }}>
+          The idea with React Native Elements is more about component structure than actual design.
+      </Text>
+        <Button
+          backgroundColor='#03A9F4'
+          // fontFamily='Lato'
+          buttonStyle={{ borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0 }}
+          title='VIEW NOW' />
+      </Card >
+    )
   }
 
   render() {
@@ -81,21 +53,7 @@ class Home extends React.Component {
       <View style={{ flex: 1 }}>
         <FlatList
           data={this.state.recipes}
-          renderItem={({ item }) => <Text>{item.name}</Text>}
-        />
-        <FlatList
-          data={this.state.todos}
-          renderItem={({ item }) => <Todo {...item} />}
-        />
-        <TextInput
-          placeholder={'Add TODO'}
-          value={this.state.textInput}
-          onChangeText={text => this.updateTextInput(text)}
-        />
-        <Button
-          title={'Add TODO'}
-          disabled={!this.state.textInput.length}
-          onPress={() => this.addTodo()}
+          renderItem={this.renderItem}
         />
       </View>
     )
